61c2c3d5c9f02957f63fe4ef5216b73b52f72d46
Вадим Семочкин <vadimsemochkin96@yandex.ru>
2021-12-14 23:19:06 +0300

    Добавьте файлы проекта.

---
+++ Lab 14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Animal
+    {
+        
+        public abstract string Name { get; set; }
+        public Animal(string Name)
+        {
+            Name="nikto";
+        }
+        public abstract string Say();
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("имя:{0}", Name);
+            Say();
+        }
+        
+    }
+    class Cat:Animal
+    {
+        public string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = "Кошка";
+            }
+
+        }
+
+        public override string Say()
+        {
+            Console.WriteLine("Говорит Мяу");
+        }
+    }
+    class Dog:Animal
+    {
+        public string name;
+    }
+}
